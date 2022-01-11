3d0e2f62f2c1fe74c3dcae9bf989d68d0b9f7a3b
Islam <islam.fawzy25@hotmail.com>
2022-01-11 11:03:13 +0100

    added my name logo

--- src/components/navBar/Nav.js
+++ src/components/navBar/Nav.js
@@ -1,24 +1,31 @@
 import React from "react";
 import "./nav.css"
+import logo from "./logo.jpg"
 
 export default function NavBar() {
 
     return (
+
         <div className="nav-container">
             <div>
-                <a href="#about">About</a>
+                <img src={logo} alt="my-name-logo" />
             </div>
-            <div>
-                <a href="#skills">Skills</a>
-            </div>
-            <div>
-                <a href="#projects">Projects</a>
-            </div>
-            <div>
-                <a href="#contact">Contact</a>
+
+            <div className="nav-btn-container">
+                <div>
+                    <a href="#about">About</a>
+                </div>
+                <div>
+                    <a href="#skills">Skills</a>
+                </div>
+                <div>
+                    <a href="#projects">Projects</a>
+                </div>
+                <div>
+                    <a href="#contact">Contact</a>
+                </div>
             </div>
         </div>
-
 
     )
 }
