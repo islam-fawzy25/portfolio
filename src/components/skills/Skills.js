9bd1445add5b57bf96812177490daf750ac7ad5f
Islam <islam.fawzy25@hotmail.com>
2022-01-17 11:03:51 +0100

    logos added

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,6 +1,23 @@
 import React from "react";
 import "./skills.css"
-import ProgressBar from "./ProgressBar";
+import CircleProgress from "./progrssBar/ProgressCircle";
+import git from "../../logos/git.png"
+import html from "../../logos/html-5.png"
+import css from "../../logos/css.png"
+import js from "../../logos/js-file.png"
+import csharp from "../../logos/hashtag.png"
+import mysql from "../../logos/mysql.png"
+import nodejs from "../../logos/.png"
+import react from "../../logos/react.png"
+import typescript from "../../logos/typescript.png"
+import jquary from "../../logos/jquary.png"
+import angular from "../../logos/angular.png"
+import dotnet from "../../logos/dotnet1.png"
+import sql from "../../logos/sql3.png"
+
+
+
+
 export default function Skills() {
 
     return (
@@ -10,29 +27,24 @@ export default function Skills() {
             </div>
             <div className="skills-container">
                 <div className="logo-container">
-                    <div >HTML5</div>
-                    <div >CSS3</div>
-                    <div >JavaScript(ES6)</div>
-                    <div >React</div>
-                    <div >NodeJS</div>
-                    <div >MYSQL</div>
+                    <CircleProgress src={html} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={css} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={git} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={react} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={js} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={nodejs} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={jquary} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={typescript} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={dotnet} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={sql} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={mysql} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={csharp} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={csharp} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={csharp} value={50 / 100} text={"HTML5"} />
+                    <CircleProgress src={angular} value={50 / 100} text={"HTML5"} />
 
-                    <div >Bootstrap</div>
-                  
-                    <div >Git</div>
-                </div>
-                <div className="logo-container">
-                    <div >C#</div>
-                    <div >.NET </div>
-                    <div >SQL server</div>
-                    <div >AngularJS</div>
-                    <div >Blazor</div>
-                    <div >TypeScript</div>
-                    <div >JQuery</div>
-                    <div >Umbraco</div>
                 </div>
                 <div>
-                <ProgressBar/>
                 </div>
             </div>
         </div>
