c157177c19ad65ae2ef69e7b22b752b230c87cb1
Islam <islam.fawzy25@hotmail.com>
2022-01-30 13:32:55 +0100

    add weather project and add singleProject component

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,8 +3,7 @@ import "./projects.css"
 import mealSharing from "../../images/meal-sharing.png"
 import simply from "../../images/simply-name-it.png"
 import weatherApp from "../../images/weatherApp.png"
-import github from "../../logos/github5.png"
-import web from "../../logos/click.png"
+import ProjectComponent from "./SingleProject";
 
 export default function Projects() {
     return (
@@ -13,84 +12,30 @@ export default function Projects() {
                 <h3 >Projects</h3>
             </div>
             <div className="projects-container">
-                <div className="single-project-container">
-                    <img className="project-pic" src={mealSharing} alt="meal.-sharing-pic" />
-                    <div className="project-description-container">
-                        <h2 > Meal sharing</h2>
-                        <div> A platform for sharing meals.<br /> where you can add, reserve or review meals.
-                        </div>
-                        <div className="tech-container">
-                            <span> Javascript </span>
-                            <span> React </span>
-                            <span> NodeJS </span>
-                            <span> MYSQL </span>
-                            <span> Express</span>
-                            <span> Knex </span>
-                            <span> Bootstrap </span>
-                        </div>
-                        <div className="project-logo-container">
-                            <a href="https://github.com/islam-fawzy25/meal-sharing" target="_blank" rel=" noreferrer">
-                                <img className="project-logo" src={github} alt="github-logo" />
-                                <div>Code</div>
-                            </a>
-                            <a href="https://hyf-meal-sharing-app.herokuapp.com/" target="_blank" rel=" noreferrer">
-                                <img className="project-logo" src={web} alt="click-logo" />
-                                <div>Website</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="single-project-container">
-                    <img className="project-pic" src={simply} alt="meal.-sharing-pic" />
-                    <div className="project-description-container">
-                        <h2>Simply name it  </h2>
-                        <div> App helps you to make a ballot from your network to choose your next decision.  </div>
-                        <div className="tech-container">
-                            <span> Javascript </span>
-                            <span> React </span>
-                            <span> NodeJS </span>
-                            <span> MYSQL </span>
-                            <span> Express</span>
-                            <span> Knex </span>
-                            <span> Swagger</span>
-                            <span> Storybook </span>
-                        </div>
-                        <div className="project-logo-container">
-                            <a href="https://github.com/HackYourFuture-CPH/simply-name.it" target="_blank" rel=" noreferrer">
-                                <img className="project-logo" src={github} alt="github-logo" />
-                                <div>Code</div>
-                            </a>
-                            <a href="https://staging-simply-name-it.herokuapp.com/" target="_blank" rel=" noreferrer">
-                                <img className="project-logo" src={web} alt="click-logo" />
-                                <div>Website</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="single-project-container">
-                    <img className="project-pic" src={weatherApp} alt="weather-app-pic" />
-                    <div className="project-description-container">
-                        <h2 > Weather App</h2>
-                        <div> A platform where you can check any city weather.</div>
-                        <div className="tech-container">
-                            <span> Javascript </span>
-                            <span> HTML5 </span>
-                            <span> CSS3 </span>
-                        </div>
-                        <div className="project-logo-container">
-                            <a href="https://github.com/islam-fawzy25/Weather-App" target="_blank" rel=" noreferrer">
-                                <img className="project-logo" src={github} alt="github-logo" />
-                                <div>Code</div>
-                            </a>
-                            <a href="https://islam-fawzy25.github.io/Weather-App/" target="_blank" rel=" noreferrer">
-                                <img className="project-logo" src={web} alt="click-logo" />
-                                <div>Website</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                <ProjectComponent
+                    projectName="Weather APP"
+                    projectDescription="Weather App is a platform where you can check any city's weather  "
+                    src={weatherApp} alt="weather-app-pic"
+                    codeHref="https://github.com/islam-fawzy25/Weather-App"
+                    websiteHref="https://islam-fawzy25.github.io/Weather-App/"
+                    techStack={["HTML5", "CSS3", "JavaScript (ES6)"]}
+                />
+                <ProjectComponent
+                    projectName="Meal sharing"
+                    projectDescription="A platform for sharing meals. where you can add, reserve or review meals.  "
+                    src={mealSharing} alt="meal-sharing-pic"
+                    codeHref="https://github.com/islam-fawzy25/meal-sharing"
+                    websiteHref="https://hyf-meal-sharing-app.herokuapp.com/"
+                    techStack={["HTML5", "CSS3", "JavaScript (ES6)", "NodeJs", "Express", "KnexJs", "MYSQL", "ReactJs", "React Bootstrap"]}
+                />
+                <ProjectComponent
+                    projectName="Simply name it"
+                    projectDescription="App helps you to make a ballot from your network to choose your next decision.  "
+                    src={simply} alt="simply-pic"
+                    codeHref="https://github.com/HackYourFuture-CPH/simply-name.it"
+                    websiteHref="https://staging-simply-name-it.herokuapp.com/"
+                    techStack={["HTML5", "CSS3", "JavaScript (ES6)", "NodeJs", "Express", "KnexJs", "MYSQL", "ReactJs", "Storybook","Swagger"]}
+                />
             </div>
         </div>
     )
