cb0af13e118d5e1a9ca39959181dce83e6a6ae35
Islam <islam.fawzy25@hotmail.com>
2022-11-25 20:33:45 +0100

    delete not working projects url

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -21,7 +21,6 @@ export default function Projects() {
                     projectDescription="App helps you to make a ballot from your network to choose your next decision.  "
                     src={simply} alt="simply-pic"
                     codeHref="https://github.com/HackYourFuture-CPH/simply-name.it"
-                    websiteHref="https://staging-simply-name-it.herokuapp.com/"
                     techStack={["HTML5", "CSS3", "JavaScript (ES6)", "NodeJs", "Express", "KnexJs", "MYSQL", "ReactJs", "Storybook", "Swagger"]}
                 />
                 <ProjectComponent
@@ -29,7 +28,6 @@ export default function Projects() {
                     projectDescription="A platform for sharing meals. where you can add, reserve or review meals.  "
                     src={mealSharing} alt="meal-sharing-pic"
                     codeHref="https://github.com/islam-fawzy25/meal-sharing"
-                    websiteHref="https://hyf-meal-sharing-app.herokuapp.com/"
                     techStack={["HTML5", "CSS3", "JavaScript (ES6)", "NodeJs", "Express", "KnexJs", "MYSQL", "ReactJs", "React Bootstrap"]}
                 />
                 <ProjectComponent
